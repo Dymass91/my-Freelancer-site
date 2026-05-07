acc101aefbe82d946f4ade1edd941435ac6a8e82
Dymass91 <matyszczak24@gmail.com>
2026-05-07 23:25:31 +0200

    auto-deploy: sync changes

--- JS/swiper.js
+++ JS/swiper.js
@@ -1,14 +1,20 @@
+var isMobileScreen = window.innerWidth < 1025;
+
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 15,
+        rotate: isMobileScreen ? 0 : 15,
         stretch: 0,
-        depth: 80,
+        depth: isMobileScreen ? 30 : 80,
         modifier: 1,
         slideShadows: false,
     },
     loop: true,
+    lazy: {
+        loadPrevNext: true,
+        loadPrevNextAmount: 1,
+    },
 });
