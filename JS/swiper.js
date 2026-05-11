7b1574e30f146ba2af043ff7e55f6003f8d74866
Dymass91 <matyszczak24@gmail.com>
2026-05-11 23:50:26 +0200

    auto-deploy: sync changes

--- JS/swiper.js
+++ JS/swiper.js
@@ -1,16 +1,16 @@
-var isMobileScreen = window.innerWidth < 1025;
-
-var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: isMobileScreen ? 0 : 15,
-        stretch: 0,
-        depth: isMobileScreen ? 30 : 80,
-        modifier: 1,
-        slideShadows: false,
-    },
-    loop: true,
-});
+if (window.innerWidth >= 768) {
+    new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 15,
+            stretch: 0,
+            depth: 80,
+            modifier: 1,
+            slideShadows: false,
+        },
+        loop: true,
+    });
+}
